3cb061af1cf544f3f00f02803c05ddcf0982a170
Maxim Kouprianov <maxim@kouprianov.com>
2014-04-09 04:23:11 +0400

    A test

--- mimetex_test.go
+++ mimetex_test.go
@@ -2,12 +2,21 @@ package mimetex
 
 import "testing"
 
-// I'm not sure if mimeTeX renders images hardware-independently,
-// so assume everythining is OK, otherwise head to example/main.go
+var expr string = `\mathcal{M}^{ax}_{in}\llbracket\gamma, \delta\rrbracket`
 
-// But we'll do a benchmark.
+// I'm not sure if mimeTeX renders images hardware-independently,
+// so assume everythining is OK, otherwise test manually with example/main.go
+func TestRender(t *testing.T) {
+	data, err := RenderGifBytes(expr, 5)
+	if err != nil {
+		t.Fatalf("mimtex error: %s", err.Error())
+	}
+	if len(data) < 1 {
+		t.Fatalf("empty results")
+	}
+}
+
 func BenchmarkRender(b *testing.B) {
-	expr := `\mathcal{M}^{ax}_{in}\llbracket\gamma, \delta\rrbracket`
 	for i := 0; i < b.N; i++ {
 		RenderGifBytes(expr, 5)
 	}
